b5f7b7ccbad90f6943664230745977408a138b4a
Alexey Tsymlov <alexey.tsymlov@gmail.com>
2015-03-03 21:47:22 +0300

    Properties were added.

--- Swift-Classes&Structures.playground/section-1.swift
+++ Swift-Classes&Structures.playground/section-1.swift
@@ -24,3 +24,27 @@ println("Welcome to \(chat.name)")
 println("We have got \(chat.members.count) members")
 
 let anotherMessage = Message(text: "Hi", recipientID: 1, senderID: 2)
+
+struct User {
+    var firstName: String
+    var lastName: String
+    let ID: Int
+    var fullName: String {
+        get{
+            return firstName + " " + lastName
+        }
+        set(newFullName){
+            firstName = newFullName
+            lastName = ""
+        }
+    }
+}
+
+var billGates = User(firstName:"Bill", lastName:"Gates", ID: 1)
+//let timCook = User() // Ошибка
+billGates.firstName = "Bill - Gay. Tss..."
+billGates.lastName = ""
+//billGates.ID = 0 // Ошибка
+
+println(billGates.fullName)
+billGates.fullName = "Bill Gates"
