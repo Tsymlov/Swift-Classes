fabc3a09fa044b3c8e6b4566f973c3e71f6e30c2
Alexey Tsymlov <alexey.tsymlov@gmail.com>
2015-03-03 20:49:57 +0300

    Class and Structure were finished.

--- Swift-Classes&Structures.playground/section-1.swift
+++ Swift-Classes&Structures.playground/section-1.swift
@@ -1,6 +1,26 @@
 class SomeClass {
     // properties and methods
 }
-struct SomeStruc {
+struct SomeStruct {
     // properties and methods
 }
+class Chat {
+    var name = ""
+    var members = Array<String>()
+}
+struct Message {
+    var text = ""
+    var recipientID = 0
+    var senderID = 0
+}
+
+let someClassInstance = SomeClass()
+let someStructInstance = SomeStruct()
+let chat = Chat()
+let message = Message()
+
+chat.name = "Bla Bla Room"
+println("Welcome to \(chat.name)")
+println("We have got \(chat.members.count) members")
+
+let anotherMessage = Message(text: "Hi", recipientID: 1, senderID: 2)
