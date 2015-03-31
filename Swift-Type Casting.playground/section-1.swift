06f7e9dcfdd6b0d76035a1b57b5f60da83bb6ff5
Alexey Tsymlov <alexey.tsymlov@gmail.com>
2015-03-31 11:46:58 +0300

    Added Type casting

---
+++ Swift-Type Casting.playground/section-1.swift
@@ -0,0 +1,77 @@
+import Foundation
+import UIKit
+
+class ContentOperation: NSOperation {
+    var progressHandler: (Double)->() = { (d) in /* do nothing */ }
+    var completionHandler: ()->() = {/* do nothing */}
+    
+    func setProgress(progress: Double){
+        progressHandler(progress)
+    }
+}
+
+class DownloadContentOperation: ContentOperation {
+    private var blobID: UInt
+    
+    init(blobID: UInt){
+        self.blobID = blobID
+        super.init()
+    }
+    
+    override func main() {
+        //downloading smth
+    }
+}
+
+class UploadContentOperation: ContentOperation {
+    private var fileName: String
+    private var contentType: String
+    
+    init(fileName: String, contentType: String){
+        self.fileName = fileName
+        self.contentType = contentType
+        super.init()
+    }
+    
+    override func main(){
+        //upload smth
+    }
+}
+
+let ops = [
+    UploadContentOperation(fileName: "attachment", contentType: "image"),
+    UploadContentOperation(fileName: "archivedAttachment", contentType: "image"),
+    DownloadContentOperation(blobID: 1234),
+    UploadContentOperation(fileName: "post", contentType: "video")
+]
+
+var downloadingOps = [DownloadContentOperation]()
+var uploadingOps = [UploadContentOperation]()
+
+for op in ops{
+    if op is DownloadContentOperation {
+        downloadingOps.append(op as DownloadContentOperation)
+//        downloadingOps.append(op as UploadContentOperation) // Ошибка
+        println(op as? UploadContentOperation)
+    }else if op is UploadContentOperation{
+        uploadingOps.append(op as UploadContentOperation)
+    }
+//    if op is NSOperation{
+//        //Ошибка NSOperation is not a subtype os ContentOperation
+//    }
+    let o = op as NSOperation
+    
+}
+
+struct s {}
+class c {}
+var a = [AnyObject]()
+a.append(c())
+//a.append(s()) //Type s does not conform to protocol AnyObject
+
+var ar = [Any]()
+ar.append(c())
+ar.append(s())
+ar.append({(name: String)-> String in
+    return "Mr. \(a)"
+})
