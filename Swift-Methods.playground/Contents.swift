a6ae0d8ed2e81058a52826bf4bed1deb9e9ac972
Alexey Tsymlov <alexey.tsymlov@gmail.com>
2015-04-15 17:19:14 +0300

    Finished methods chapter.

--- Swift-Methods.playground/Contents.swift
+++ Swift-Methods.playground/Contents.swift
@@ -18,3 +18,51 @@ func updateContacts(success:([User])->(), failure: (NSError!)->()){
     //do smth with contacts
 }
 updateContacts({a in println(a)}, {e in println(e)})
+
+struct Data {
+    var day: Int
+    var month: Int
+    var year: Int
+    
+    mutating func setNextDay(){
+        ++day
+    }
+    mutating func setNextMonth(){
+        self = Data(day: day, month: month+1, year: year)
+    }
+}
+
+var day = Data(day: 15,month: 04,year: 2015)
+day.setNextDay()
+day.setNextMonth()
+
+enum Switch{
+    case On, Off
+    mutating func turn(){
+        if self == On{
+            self = Off
+        }else{
+            self = On
+        }
+    }
+    static func load()->Switch{
+        //load switch from local storage
+        return Switch.Off
+    }
+    
+    static func loadQuick()->Switch{
+        return self.load()
+    }
+}
+
+var sw = Switch.Off
+sw.turn()// is On
+
+import CoreData
+class Profile: NSManagedObject {
+    class func entity()->NSEntityDescription{
+        // return some entity
+        return NSEntityDescription()
+    }
+}
+Profile.entity()
