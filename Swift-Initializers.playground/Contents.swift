d937b5e2fa0317753619429589fe461a7835ce8a
Alexey Tsymlov <alexey.tsymlov@gmail.com>
2015-05-06 09:19:02 +0300

    Added Initializers.

---
+++ Swift-Initializers.playground/Contents.swift
@@ -0,0 +1,10 @@
+class Message {
+//    var text: String
+//    init(){
+//        text = ""
+//    }
+    var text = ""
+}
+
+var msg = Message()
+println("Current text length = \(count(msg.text))")
