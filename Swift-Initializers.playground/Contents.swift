911b99464402270e4f333ccba68bbe0d30dc1afa
Alexey Tsymlov <alexey.tsymlov@gmail.com>
2015-05-07 12:40:41 +0300

    Added initialization customization.

--- Swift-Initializers.playground/Contents.swift
+++ Swift-Initializers.playground/Contents.swift
@@ -8,3 +8,43 @@ class Message {
 
 var msg = Message()
 println("Current text length = \(count(msg.text))")
+
+
+class User {
+    var firstName: String
+    var lastName: String
+    
+    init(){
+        firstName = ""
+        lastName = ""
+    }
+    
+    init(firstName: String, lastName: String){
+        self.firstName = firstName
+        self.lastName = lastName
+    }
+    
+    var description: String {
+        return "\(firstName) \(lastName)"
+    }
+}
+
+let billGates = User(firstName: "Bill", lastName: "Gates")
+var emptyUser = User()
+println(billGates.description)
+println(emptyUser.description)
+
+class Chat {
+    let id: Int
+
+    init(id: Int){
+        self.id = id
+    }
+}
+
+class ChatRoom: Chat {
+    init(id: Int){
+        super.init(id: id)
+//        id = 7 //Error: Cannot assign to 'let' value 'id'
+    }
+}
