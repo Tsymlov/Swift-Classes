ea1748dc8c602017a7b12fc8755b854cb222c687
Alexey Tsymlov <alexey.tsymlov@gmail.com>
2015-07-02 14:02:55 +0300

    Added tuples, value binding and where for switch.

--- Swift-Control Flow.playground/section-1.swift
+++ Swift-Control Flow.playground/section-1.swift
@@ -4,6 +4,9 @@ for i in 1...100{
 for _ in 1...100{
     // do something 100 times
 }
+for var i = 0; i<100; ++i{
+    // do something 100 times
+}
 
 var greeting: String
 let localization = "English"
@@ -32,3 +35,29 @@ default:
 }
 
 println(tempOfWater)
+
+let someColor = (1.0, 1.0, 1.0, 0.1)
+switch someColor{
+case (0.0, 0.0, 0.0, 0.0):
+    println("It's white.")
+case (_, _, _, 0.0):
+    println("It's transparent.)")
+case (let r, let g, let b, 0.0...0.2):
+    println("It's more invisible. (r,g,b)=(\(r),\(g),\(b))")
+case let (r, g, b, a):
+    println("It's visible color. (r,g,b,a)=(\(r),\(g),\(b),\(a))")
+}
+
+let anotherColor = (1.0, 1.0, 1.0, 10.0)
+switch anotherColor{
+case let (r,_,_,_) where !((0.0...1.0) ~= r):
+    println("Wrong value! r = \(r)")
+case let (_,g,_,_) where !((0.0...1.0) ~= g):
+    println("Wrong value! g = \(g)")
+case let (_,_,b,_) where !((0.0...1.0) ~= b):
+    println("Wrong value! b = \(b)")
+case let (_,_,_,a) where !((0.0...1.0) ~= a):
+    println("Wrong value! a = \(a)")
+default:
+    println("Checking passed.")
+}
