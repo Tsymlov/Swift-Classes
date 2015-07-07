19ce0595d20e74026965d262e81cb0f18228651e
Alexey Tsymlov <alexey.tsymlov@gmail.com>
2015-07-07 22:48:48 +0300

    Added value capturing and reference type.

--- Swift-Closures.playground/section-1.swift
+++ Swift-Closures.playground/section-1.swift
@@ -7,3 +7,21 @@ func getFullName(firstName: String, lastName: String, success: (fullName: String
     return success(fullName: fullName)
 }
 getFullName("Tim", "Cook") {"Mr. " + $0}
+
+var sum = 0
+let add: (Int)->() = {value in
+    sum += value
+}
+
+add(5)
+println(sum)//5
+add(8)
+println(sum)//13
+
+let anotherAdd = add
+anotherAdd(10)
+println(sum)//23
+
+let someTuple: (Int, ()->()) = (5, {println("Boo")})
+someTuple.1()
+someTuple.0
