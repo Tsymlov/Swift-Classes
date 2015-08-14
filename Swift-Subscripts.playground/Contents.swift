db2e51049c2f5a5ea9cf3c5ad752eb363ac6a17b
Alexey Tsymlov <alexey.tsymlov@gmail.com>
2015-08-14 12:46:35 +0300

    Finished subscript chapter.

--- Swift-Subscripts.playground/Contents.swift
+++ Swift-Subscripts.playground/Contents.swift
@@ -27,3 +27,51 @@ users.append(User(firstName: "Bill", lastName: "Gates"))
 users.append(User(firstName: "Tim", lastName: "Cook"))
 users[0].lastName = "Jobs"
 println(users[0].firstName! + " " + users[0].lastName!)
+
+
+class DataTable {
+    private var data = [[1,2,3,4,5,6,7,8,9,0],[1,2,3,4,5,6,7,8,9,0],[1,2,3,4,5,6,7,8,9,0]]
+    subscript(row: Int, column: Int)->Int{
+        get{
+            return data[row][column]
+        }
+        set{
+            data[row][column] = newValue
+        }
+    }
+}
+
+let table = DataTable()
+println(table[1, 3])
+table[2,0] = 0
+
+class PhonesStore {
+    private var phones = ["iphn": "iPhone", "smsg": "Samsung Galaxy", "nka": "Nokia 3310"]
+    
+    subscript(marking: String)-> String?{
+        return phones[marking]
+    }
+    
+    subscript(marking: String, count: Int)->(String, Int)?{
+        return phones[marking] != nil ? (phones[marking]!, count) : nil
+    }
+}
+
+let store = PhonesStore()
+
+println(store["iphn"])
+println(store["smsg", 100])
+
+class SomeClass {
+    subscript(var param: Int)->Int{
+        param++
+        return param
+    }
+    
+    subscript(params: Int...)->[Int]{
+        return params
+    }
+}
+
+let some = SomeClass()
+println(some[1,2,3,4,5,6,7])
