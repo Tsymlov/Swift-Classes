1d16009e8827c9eb2e00f0a028bce53644090b3c
Alexey Tsymlov <alexey.tsymlov@gmail.com>
2015-09-10 20:04:47 +0300

    Created Sandbox project.

---
+++ Sandbox/Sandbox/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  Sandbox
+//
+//  Created by Alexey Tsymlov on 9/9/15.
+//  Copyright (c) 2015 Alexey Tsymlov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
