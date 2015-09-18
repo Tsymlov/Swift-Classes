b44965f0c921f1ac1bceccb044e65c93d40d2b89
Tsymlov <alexey.tsymlov@gmail.com>
2015-09-18 12:33:41 +0300

    Added Indexes and Modifying of Strings.

--- Swift-Strings and Characters.playground/Contents.swift
+++ Swift-Strings and Characters.playground/Contents.swift
@@ -34,3 +34,38 @@ else {
 
 response.hasSuffix("}")
 response.hasPrefix("{")
+
+
+
+//response[0] // Error!
+
+response[response.startIndex]
+response.characters.first
+
+response[response.endIndex.predecessor()]
+response.characters.last
+
+response[response.startIndex.successor()]
+//response.characters[2] // Error!
+
+let i = response.startIndex.advancedBy(3)
+response[i]
+
+//response[response.endIndex] //Error!
+//response[response.endIndex.successor()] //Error!
+
+for index in response.characters.indices {
+    print(response[index], terminator: "*")
+}
+
+response.insert("*", atIndex: response.startIndex)
+
+let name = "\",\nname\": \"John Appleseed\""
+response.insertContentsOf(name.characters, at: response.endIndex.predecessor().predecessor())
+
+response.removeAtIndex(response.startIndex)
+print(response)
+
+let range = response.endIndex.advancedBy(-10)..<response.endIndex
+response.removeRange(range)
+print(response)
