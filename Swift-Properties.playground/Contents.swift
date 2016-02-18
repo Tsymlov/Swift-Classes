4308252dc6caaad98e62dc0df560f86da8c6c8b5
Tsymlov <alexey.tsymlov@gmail.com>
2016-02-18 11:05:51 +0300

    Update Properties for Swift 2

---
+++ Swift-Properties.playground/Contents.swift
@@ -0,0 +1,47 @@
+import Foundation
+
+struct User {
+    var firstName: String
+    var lastName: String
+    let ID: Int
+    var fullName: String {
+        get{
+            return firstName + " " + lastName
+        }
+        set(newFullName){
+            firstName = newFullName
+            lastName = ""
+        }
+    }
+}
+
+var billGates = User(firstName:"Bill", lastName:"Gates", ID: 1)
+//let timCook = User() // Ошибка
+billGates.firstName = "Bill - Gay. Tss..."
+billGates.lastName = ""
+//billGates.ID = 0 // Ошибка
+
+print(billGates.fullName)
+billGates.fullName = "Bill Gates"
+
+class FileSystemHelper {
+    
+    lazy var fm0 = NSFileManager()
+    lazy var fm1 = NSFileManager.defaultManager()
+    lazy var fm2: NSFileManager = {return NSFileManager.defaultManager()}()
+    lazy var one = 1
+}
+
+let helper = FileSystemHelper()
+
+class Message {
+    var length: Int!
+    var text: String!{
+        willSet{
+            length = newValue.characters.count
+        }
+        didSet{
+            print(oldValue)
+        }
+    }
+}
